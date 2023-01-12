ba760b6b2d490f2dc6fb3e732776e32e86b498a1
hwangblood <hwangblood@gmail.com>
2023-01-12 20:25:54 +0800

    Install Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  content: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
